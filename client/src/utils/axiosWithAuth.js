ff64dd152656d10e7b81ca59cf96d4a66cbd07af
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-07-20 13:35:04 -0400

    lecture work-in-progress

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = ()=> {
     const token = localStorage.getItem('token');
 
     return axios.create({
-        baseURL: '',
+        baseURL: 'http://localhost:5000/api',
         headers: {
             authorization: token
         }
