37b7f7e77a3541f5b0db00d3bee2fd89464f0c9f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-07-20 13:42:48 -0400

    lecture work-in-progress

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ function App() {
             <Link onClick={logout}>Logout</Link>
           </li>
           <li>
-            <Link to="/protected">Protected Page</Link>
+            {/* <Link to="/protected">Protected Page</Link> */}
           </li>
         </ul>
 
